2f28d0c22b11c5731fa2d2831a21faadb94081cb
Josh Grossman <grossmanjoshuaj@gmail.com>
2019-06-15 17:13:39 -0500

    main functionality is good

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,90 @@
-
 var crystalsArray =[];
 var score = 0;
 var wins = 0;
 var losses = 0;
-var roundScore = 0;
 var randomNum = 0;
-var randomValue = 0;
+var diamond;
+var ruby;
+var emerald;
+var saphire;
+var roundScore = 0;
 
-function reset(){
-    roundScore = 0;
+function game(){
+    
+    
+    //when we reset the game we want to reset the random number and crystals
+    randomNum = Math.floor(Math.random() * 12 +39);
+    $("#numberRandom").html("Random Number: " + randomNum);
+
+    diamond = new crystals("diamond", "./assets/images/dmd.jpg");
+    ruby = new crystals("ruby", "./assets/images/ruby.jpg");
+    emerald = new crystals("emerald", "./assets/images/emerald.jpg");
+    saphire = new crystals("saphire", "./assets/images/saphire.jpg");
+    crystalsArray.push(diamond,ruby, emerald, saphire);
+    for(var i = 0; i < crystalsArray.length;i++){
+        // randomValue = Math.floor(Math.random() * 12 +1);
+        var crysPic = $("<img>");
+        $(crysPic).addClass("img-thumbnail number");
+        $(crysPic).attr("value", crystalsArray[i].value);
+        $(crysPic).attr("src", crystalsArray[i].pic);
+        $(crysPic).attr("style","width:130px");
+        $("#crysDisplay").append(crysPic);
+    }
+    $(".number").on("click", function(){
+        roundScore = parseInt($(".number").attr("value"));
+        $("#valueCrys").html("Crystal Value: "+ roundScore);
+        score = (parseInt(score)+ parseInt(roundScore));
+        $("#currentTotal").html("Your Current Total: "+ score);
+        console.log(roundScore);
+        console.log(score);
+        console.log(randomNum);
+        checkForWin();
+    });
+   
 }
 
-function crystals(type, value, pic) {
-    this.type = type;
-    this.value = value;
-    this.pic= pic;
-  }
+    function crystals(type, pic) {
+        this.type = type;
+        this.value = Math.floor(Math.random() * 12 +1);
+        this.pic= pic;
+    }
 
 //Initialize objects
 
-
-var diamond = new crystals("diamond", randomValue,"./assets/images/dmd.jpg");
-var ruby = new crystals("ruby",randomValue,"./assets/images/ruby.jpg");
-var emerald = new crystals("emerald", randomValue,"./assets/images/emerald.jpg");
-var saphire = new crystals("saphire", randomValue,"./assets/images/saphire.jpg");
-
-crystalsArray.push(diamond,ruby, emerald, saphire);
-
-
-for(var i = 0; i < crystalsArray.length;i++){
-    randomValue = Math.floor(Math.random() * 12 +1);
-    var crysPic = $("<img>");
-    $(crysPic).addClass("img-thumbnail number");
-    $(crysPic).attr("value", randomValue);
-    $(crysPic).attr("src", crystalsArray[i].pic);
-    $(crysPic).attr("style","width:130px");
-    $("#crysDisplay").append(crysPic);
+function reset() {
+    $("#crysDisplay").empty();
+    score = 0;
+    $("#currentTotal").html("Your Current Total: "+ score);
+    crystalsArray =[];
+    roundScore = 0;
+    $("#valueCrys").html("Crystal Value: "+ roundScore);
+    game()
 }
 
-
-
-    $(".number").on("click", function(){
-        roundScore = parseInt($(".number").attr("value"));
-        score  += roundScore;
-        console.log(roundScore);
-        console.log(score);
-        reset();
-        console.log(roundScore);
-    });
 
 function checkForWin(){
+    if(score < randomNum){
+        $("#gameMessages").html("Click another crystal <br> to keep going!");
+             
+        }
+    else if (score > randomNum){
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        $("#gameMessages").html("You Lost The Round :(");
+        reset()
+      
+        
+        }
+    else if(score === randomNum){
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        $("#gameMessages").html("You Won This Round!");
+        reset()
+      
+        
+        }
+    }
 
-}
+game();
+
+
